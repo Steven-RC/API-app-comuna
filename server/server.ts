fc236c39a2bf0dc2f78f71a1628b2092ee06e86b
Steven Rosales <steven.rosalesc@upse.edu.ec>
2023-01-31 20:06:22 -0500

    Actualizacion hasta modificacion de Rol

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,5 @@
 import express, { Application } from 'express';
+
 import userRoutes from '../routes/usuarios';
 import cors from "cors";
 import db from '../db/connection';
@@ -8,6 +9,14 @@ import personaRoutes from '../routes/personas';
 import rolRouter from '../routes/roles';
 import comuneroRoutes from '../routes/comuneros';
 import resetPasswordRoutes from '../routes/ressetPassword';
+import asociacionesROutes from '../routes/asociaciones';
+import anioRoutes from '../routes/anio';
+import cuotaRoutes from '../routes/cuotas';
+import requisitosRoutes from '../routes/requisitos';
+import requisitosAprRoytes from '../routes/requisitos_apr';
+import facturasRoutes from '../routes/facturas';
+import cuotasFacturaRoutes from '../routes/cuotas_factura';
+import { Nlp } from 'node-nlp';
 
 
 
@@ -23,17 +32,27 @@ class Server {
         roles: '/api/roles',
         comuneros: '/api/comuneros',
         contrasenia: '/api/contrasenia',
+        asociaciones:'/api/asociaciones',
+        anio:'/api/anio',
+        cuota:'/api/cuota',
+        requisitos:'/api/requisitos',
+        requisitosApr:'/api/requisitosApr',
+        facturas:'/api/facturas',
+        cuotasFactura:'/api/cuotasFactura'
+
     }
 
 
     constructor() {
         this.app = express();//inicializamos express
+        
         this.port = process.env.PORT || '8000';//puerto por defecto
         //middlewares
         this.middlewares();//llamamos a los middlewares
         //base de datos
         this.conectarDB();//llamamos a la base de datos
         this.routes();//llamamos a las rutas
+
 
     }
     //conexion base de datos
@@ -69,6 +88,13 @@ class Server {
         this.app.use(this.apiPaths.roles, rolRouter);
         this.app.use(this.apiPaths.comuneros, comuneroRoutes);
         this.app.use(this.apiPaths.contrasenia, resetPasswordRoutes);
+        this.app.use(this.apiPaths.asociaciones, asociacionesROutes);
+        this.app.use(this.apiPaths.anio, anioRoutes);
+        this.app.use(this.apiPaths.cuota, cuotaRoutes);
+        this.app.use(this.apiPaths.requisitos, requisitosRoutes);
+        this.app.use(this.apiPaths.requisitosApr, requisitosAprRoytes);
+        this.app.use(this.apiPaths.facturas, facturasRoutes);
+        this.app.use(this.apiPaths.cuotasFactura, cuotasFacturaRoutes);
     }
 
 
@@ -79,6 +105,14 @@ class Server {
             console.log('Servidor corriendo en el puerto ' + this.port);
         });
 
+    }
+
+    nlp(){
+        const nlp = new Nlp();
+        nlp.process('I want to buy a red car', (err, result) => {
+            console.log(result);
+        }
+        );
     }
 
 }
